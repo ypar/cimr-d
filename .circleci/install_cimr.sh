8a9cda0cb0e2b4961640b0c7003305ceffca915b
cimrroot <cimrroot@gmail.com>
2019-06-12 11:09:19 -0400

    Run git-lfs in cimr-d

--- .circleci/install_cimr.sh
+++ .circleci/install_cimr.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
+# Install cimr package
 set -e -x
 
-# Install git-lfs
-cd /tmp
-wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.2/git-lfs-linux-386-v2.7.2.tar.gz
-mkdir -p git-lfs
-cd git-lfs
-tar xzf ../git-lfs-linux-386-v2.7.2.tar.gz
-sudo ./install.sh
-
-# Install cimr package
 cd ~/
 git clone https://github.com/greenelab/cimr.git
 cd cimr
