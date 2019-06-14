2d84ab8210917bcf266d6b3c60f53ade759792e1
dongbohu <dongbo.hu@gmail.com>
2019-06-14 14:05:59 -0400

    Install git-lfs in deployment step

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -12,11 +12,22 @@ git config --global user.email "cimrroot@gmail.com"
 git config --global user.name "cimrroot"
 git config --global push.default simple
 
+# Install git-lfs
+cd /tmp
+wget https://github.com/git-lfs/git-lfs/releases/download/v2.7.2/git-lfs-linux-386-v2.7.2.tar.gz
+mkdir -p git-lfs
+cd git-lfs
+tar xzf ../git-lfs-linux-386-v2.7.2.tar.gz
+sudo ./install.sh
+
+cd ~/cimr-d
+git lfs install 
+
 # Sync files in "submitted_data" directory to private S3 bucket "cimr-root"
-aws s3 sync ~/cimr-d/submitted_data s3://cimr-root
+aws s3 sync submitted_data s3://cimr-root
 
 # Sync files in "processed_data" directory to public S3 bucket "cimr-d"
-aws s3 sync ~/cimr-d/processed_data s3://cimr-d
+aws s3 sync processed_data s3://cimr-d
 
 # Remove submitted_data from git and commit w/o CircleCI
 git rm -rf submitted_data/*
