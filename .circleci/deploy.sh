5623e6609cd34c0816a6c2e4b4eb3e4af076c280
dongbohu <dongbo.hu@gmail.com>
2019-08-22 11:07:09 -0400

    Avoid duplicate output filenames in public S3 bucket (#33)

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -33,6 +33,8 @@ if [ $PR_NUMBER == 'null' ]; then
     exit 0
 fi
 
+PR_STR="PR-${PR_NUMBER}"
+
 # If we are merging a PR, but the indicator object is not found in S3 bucket,
 # data processing must either fail or not start at all, so we exit too.
 INDICATOR_FIELNAME="submitted_data/request.handled"
@@ -47,14 +49,45 @@ sudo pip install --upgrade pip
 # Install awscli to make "aws" command available
 sudo pip install awscli
 
-# Move files in S3 buckets from temporary to permanent locations.
-aws s3 sync submitted_data/  s3://cimr-root/PR-${PR_NUMBER}/ --exclude "request.handled"
-aws s3 sync processed_data/  s3://cimr-d/
+# Copy "processed_data" to "cimr-d" bucket (public).
+# "PR-<n>" is inserted in filenames to avoid duplicates.
+OUTPUT_FILES=$(find processed_data -type f)
+for f in ${OUTPUT_FILES}; do
+    g=$(echo $f | cut -d'/' -f'2-')    # strip "processed_data/" from $f
+    S3DIR=$(dirname $g)                # S3 object's dir name
+
+    g_base=$(basename $g)              # base filename
+    tokens=(${g_base//./ })            # split $g_base into an array using '/'
+    LEN="${#tokens[@]}"                # number of items in tokens
+
+    if [ $LEN -eq 1 ]; then
+	# "foo" will become "foo-PR-n"
+	S3BASE="${g_base}-${PR_STR}"
+    elif [ $LEN -eq 2 ]; then
+	# "foo.ext1" will become "foo-PR-n.ext1"
+	S3BASE="${tokens[0]}-${PR_STR}.${tokens[1]}"
+    else
+	# "foo.ext2.ext1" will become "foo-PR-n.ext2.ext1"
+	S3BASE="${tokens[0]}"
+	for ((i = 1; i < $LEN - 2; i++)); do
+	    S3BASE="${S3BASE}.${tokens[$i]}"
+	done
+
+	LEN_m2=$(expr $LEN - 2)        # $LEN - 2
+	LEN_m1=$(expr $LEN - 1)        # $LEN - 1
+	S3BASE="$S3BASE-${PR_STR}.${tokens[$LEN_m2]}.${tokens[$LEN_m1]}"
+    fi
+
+    S3NAME="${S3DIR}/$S3BASE"
+    aws s3 cp $f s3://cimr-d/${S3NAME}
+done
+
+# Copy "submitted_data" to "cimr-root" bucket (private)
+aws s3 sync submitted_data/ s3://cimr-root/${PR_STR}/ --exclude "request.handled"
 
 # Move submitted YAML files to "processed/" sub-dir
-mkdir -p processed/PR-${PR_NUMBER}/
-git mv -k submitted/*.yml submitted/*.yaml processed/PR-${PR_NUMBER}/
-git commit -m "CircleCI: Save requests to processed/ dir [skip ci]"
+mkdir -p processed/${PR_STR}/
+git mv -k submitted/*.yml submitted/*.yaml processed/${PR_STR}/
 
 # Update "processed/README.md", which lists all files in "cimr-d" S3 bucket
 aws s3 ls cimr-d --recursive --human-readable > processed/s3_list.txt
@@ -65,6 +98,6 @@ git add processed/README.md
 git add cimr-d_catalog.txt
 
 # Commit changes and push them to remote "master" branch
-git commit -m "Update processed/REAME.md and cimr-d_catalog.txt [skip ci]"
+git commit -m "CircleCI: Save processed requests [skip ci]"
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git push --force --quiet origin master
